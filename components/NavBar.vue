7e0ec026bc900cab07b14d38e9e2e380e746b071
h662 <godh662@gmail.com>
2020-02-07 15:09:47 +0900

    setting navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,21 +1,39 @@
 <template>
-  <v-container>
+  <v-container class="ma-0">
     <v-row>
-      <v-col :cols="10">
-        <v-row>
-          <v-col>logo</v-col>
-          <v-col :cols="10">
-            NavContent
-          </v-col>
-        </v-row>
+      <v-col class="d-flex justify-center">
+        logo
       </v-col>
-      <v-col>
-        <v-row>
-          <v-col>
-            NavEnd
-          </v-col>
-        </v-row>
+      <v-col :cols="10" class="d-flex justify-center">
+        <span v-for="item in menuItems" :key="item.title" class="mx-9">
+          <nuxt-link :to="item.path">{{ item.title }}</nuxt-link>
+        </span>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuItems: [
+        { title: 'Home', path: '/' }, { title: 'About', path: '/about' },
+        { title: 'Skill', path: '/skill' }, { title: 'Project', path: '/project' },
+        { title: 'Contact', path: '/contact' } ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    background: #1D976C;
+    background: -webkit-linear-gradient(to right, #93F9B9, #1D976C);
+    background: linear-gradient(to right, #93F9B9, #1D976C);
+  }
+  a {
+    text-decoration: none;
+    color: white !important;
+  }
+</style>
